944146070021b6ac2138e23632caea8ebd372fe7
198ai <leishengle1964113@yahoo.co.jp>
2022-02-07 23:52:52 +0900

    学习

--- lib/model/eventsToJoson.dart
+++ lib/model/eventsToJoson.dart
@@ -25,28 +25,3 @@ class EventsToJson {
     return data;
   }
 }
-
-// class Events {
-//   String? eventTitle;
-//   String? eventDescp;
-//   String? alarm;
-//   int? alarmId;
-
-//   Events({this.eventTitle, this.eventDescp, this.alarm, this.alarmId});
-
-//   Events.fromJson(Map<String, dynamic> json) {
-//     eventTitle = json['eventTitle'];
-//     eventDescp = json['eventDescp'];
-//     alarm = json['alarm'];
-//     alarmId = json['alarmId'];
-//   }
-
-//   Map<String, dynamic> toJson() {
-//     final Map<String, dynamic> data = new Map<String, dynamic>();
-//     data['eventTitle'] = this.eventTitle;
-//     data['eventDescp'] = this.eventDescp;
-//     data['alarm'] = this.alarm;
-//     data['alarmId'] = this.alarmId;
-//     return data;
-//   }
-// }
