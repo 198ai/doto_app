4c3f1869e86e901719f04cda88107d6c2cad29a3
198ai <leishengle1964113@yahoo.co.jp>
2022-02-19 15:51:16 +0900

    色変更

--- lib/widget/drawer.dart
+++ lib/widget/drawer.dart
@@ -202,7 +202,9 @@ class _drawerEX extends State<drawerEX> {
                                 onPressed: () {
                                   Navigator.pop(context);
                                 },
-                                child: const Text('キャンセル'),
+                                child: const Text('キャンセル', 
+                                style: TextStyle(
+                                        color: Colors.green,)),
                               ),
                               TextButton(
                                 onPressed: () async {
@@ -214,7 +216,9 @@ class _drawerEX extends State<drawerEX> {
                                   prefs.remove("userdata");
                                   Navigator.pop(context);
                                 },
-                                child: const Text('確認'),
+                                child: const Text('確認' ,
+                                style: TextStyle(
+                                        color: Colors.green,)),
                               ),
                             ],
                           );
